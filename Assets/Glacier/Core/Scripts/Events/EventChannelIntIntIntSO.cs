d651288bec797d7c7620e0e2b6621e9a845f0a89
snowblinkstudiosdeveloper <developer@snowblink.io>
2023-07-02 01:48:23 +0800

    Update EventChannel for Ints filename

--- Assets/Glacier/Core/Scripts/Events/EventChannelIntIntIntSO.cs
+++ Assets/Glacier/Core/Scripts/Events/EventChannelIntIntIntSO.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 namespace Glacier.Core.Events {
-    [CreateAssetMenu(menuName="Glacier/Core/Events/EventChannel(Int)", fileName="Name_Int_Int_Int_EventChannel")]
+    [CreateAssetMenu(menuName="Glacier/Core/Events/EventChannel(Int, Int, Int)", fileName="Name_Int_Int_Int_EventChannel")]
     public class EventChannelIntIntIntSO : EventChannelSO<int, int, int> {
     }
 }
